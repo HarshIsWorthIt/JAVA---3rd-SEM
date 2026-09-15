17bf5d702de6fd2dc9b8c79a88e9f1a79c240e85
Harsh Mishra <lavaajios@gmail.com>
2026-09-15 11:14:29 +0530

    successfully committed

--- MaxMatrix.java
+++ MaxMatrix.java
@@ -2,16 +2,27 @@ import java.util.Scanner;
 
 public class MaxMatrix {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter rows: ");
         int rows = sc.nextInt();
+
+        System.out.print("Enter columns: ");
         int cols = sc.nextInt();
+
         int[][] a = new int[rows][cols];
+
+        System.out.println("Enter matrix elements:");
+
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 a[i][j] = sc.nextInt();
             }
         }
+
         int max = a[0][0];
+
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 if (a[i][j] > max) {
@@ -19,7 +30,9 @@ public class MaxMatrix {
                 }
             }
         }
+
         System.out.println("Maximum element = " + max);
+
         sc.close();
     }
 }
